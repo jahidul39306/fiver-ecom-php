617097ab080b1a3c9712d7f54f970c00a1ac995c
jahidul39306 <72722599+jahidul39306@users.noreply.github.com>
2023-02-07 14:29:11 +0600

    database updated

--- newshop.sql
+++ newshop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 04, 2023 at 08:58 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: Feb 07, 2023 at 09:26 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,7 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `UserName`, `Pass`, `FirstName`, `Email`, `PhoneNum`) VALUES
 (1, 'ahm232', '1234', 'ali', 'ali@gmail.com', '0598768898'),
-(2, 'abc123', '3213', 'bader', 'bader33@gmail.com', '0577777898'),
+(2, 'abc123', '1234', 'bader', 'bader33@gmail.com', '0577777898'),
 (3, 'aed234', '4313', 'mustafa', 'musta45@gmail.com', '0542421354'),
 (4, 'www333', '5678', 'abdul', 'abdul123@gmail.com', '0599987877'),
 (5, 'sad22', '4564', 'ibrahim', 'ibrahim@gmail.com', '0544443211'),
@@ -90,24 +90,28 @@ CREATE TABLE `customer` (
   `FirstName` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
   `Email` varchar(32) NOT NULL,
-  `PhoneNum` varchar(10) NOT NULL
+  `PhoneNum` varchar(10) NOT NULL,
+  `UserName` varchar(50) NOT NULL,
+  `Pass` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`id`, `OrderNum`, `LastName`, `FirstName`, `country`, `Email`, `PhoneNum`) VALUES
-(1, 344, 'ali', 'mohammed', 'dammam', 'moh222@gmail.com', '+539987898'),
-(2, 347, 'micv', 'alex', 'us', 'alex3232@gmail.com', '+153453456'),
-(3, 645, 'ali', 'khalid', 'khobar', 'kal777@gmail.com', '+535437898'),
-(4, 345, 'ahmed', 'bader', 'dammam', 'bad212@gmail.com', '+535685898'),
-(5, 678, 'swe', 'felix', 'mexico', 'fswe22@gmail.com', '+521234589'),
-(6, 143, 'ali', 'ahmed', 'dammam', 'ali99@gmail.com', '+539457898'),
-(7, 321, 'john', 'mik', 'paris', 'mik@gmail.com', '+333993246'),
-(8, 743, 'ali', 'abdullah', 'dammam', 'aaa@gmail.com', '+539876843'),
-(9, 567, 'mohammed', 'ahmed', 'dubai', 'moh112@gmail.com', '+971909888'),
-(10, 867, 'ali', 'hassan', 'dammam', 'hass@gmail.com', '+539435666');
+INSERT INTO `customer` (`id`, `OrderNum`, `LastName`, `FirstName`, `country`, `Email`, `PhoneNum`, `UserName`, `Pass`) VALUES
+(1, 344, 'ali', 'mohammed', 'dammam', 'moh222@gmail.com', '+539987898', '', ''),
+(2, 347, 'micv', 'alex', 'us', 'alex3232@gmail.com', '+153453456', '', ''),
+(3, 645, 'ali', 'khalid', 'khobar', 'kal777@gmail.com', '+535437898', '', ''),
+(4, 345, 'ahmed', 'bader', 'dammam', 'bad212@gmail.com', '+535685898', '', ''),
+(5, 678, 'swe', 'felix', 'mexico', 'fswe22@gmail.com', '+521234589', '', ''),
+(6, 143, 'ali', 'ahmed', 'dammam', 'ali99@gmail.com', '+539457898', '', ''),
+(7, 321, 'john', 'mik', 'paris', 'mik@gmail.com', '+333993246', '', ''),
+(8, 743, 'ali', 'abdullah', 'dammam', 'aaa@gmail.com', '+539876843', '', ''),
+(9, 567, 'mohammed', 'ahmed', 'dubai', 'moh112@gmail.com', '+971909888', '', ''),
+(10, 867, 'ali', 'hassan', 'dammam', 'hass@gmail.com', '+539435666', '', ''),
+(11, 0, NULL, 'demo', '', 'demo@gmail.com', 'demo', 'demo', 'demo'),
+(12, 0, NULL, 'customer', '', 'customer@gamil.com', 'customer', 'customer', 'customer');
 
 -- --------------------------------------------------------
 
@@ -221,7 +225,7 @@ ALTER TABLE `cpu`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `gpu`
